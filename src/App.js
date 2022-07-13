590d2321481c2348ac616ffde0d5a26e28dd5def
pablo pelardas <pablo.pelardas@gmail.com>
2022-07-13 14:05:43 -0300

    fix(client): remove loader from app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
-import Loader from './components/Loader/Loader';
 
 function App() {
   return (
@@ -51,7 +50,6 @@ function App() {
             React Redux
           </a>
         </span>
-        <Loader />
       </header>
     </div>
   );
