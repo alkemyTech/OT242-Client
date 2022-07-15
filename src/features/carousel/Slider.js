3c45078f3eaef2a957a0679a8fb1e80f81cd213f
manubautista <manubautista2009@gmail.com>
2022-07-14 23:10:55 -0300

    Some styling added to Slider component

--- src/features/carousel/Slider.js
+++ src/features/carousel/Slider.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Carousel } from "react-bootstrap"
+import '../carousel/Slider.css'
 
 export default function Slider() {
     return (
@@ -7,39 +8,39 @@ export default function Slider() {
             <Carousel variant='dark'>
                 <Carousel.Item>
                     <img
-                        className="d-block w-100"
-                        src="https://euskalnews.com/wp-content/uploads/2021/10/ong-agenda-2030.jpg"
+                        className=""
+                        src="https://logos.flamingtext.com/City-Logos/Ong-Logo.png"
                         alt="Misión"
                     />
                     <Carousel.Caption>
-                        <h3>Misión</h3>
-                        <p>Trabajar articuladamente con los distintos aspectos de la vida de las
+                        <h1 className='text'>Misión</h1>
+                        <p className='text'>Trabajar articuladamente con los distintos aspectos de la vida de las
                             familias.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
-                        className="d-block w-100"
+                        className=""
                         src="https://blog.digitalegia.com/hubfs/dinamicas-para-impulsar-ONG-redes-sociales.png"
                         alt="Visión"
                     />
 
                     <Carousel.Caption>
-                        <h3>Visión</h3>
-                        <p>Mejorar la calidad de vida de niños y familias en situación de
+                        <h1 className='text'>Visión</h1>
+                        <p className='text'>Mejorar la calidad de vida de niños y familias en situación de
                             vulnerabilidad en el barrio La Cava.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
-                        className="d-block w-100"
+                        className=""
                         src="https://i.blogs.es/7cb90c/giving_week_2018-social-tm_1_copy_1.max-1000x1000/1366_2000.jpg"
                         alt="Nosotros"
                     />
 
                     <Carousel.Caption>
-                        <h3>Nosotros</h3>
-                        <p>
+                        <h1 className='text'>Nosotros</h1>
+                        <p className='text'>
                             Desde 1997 vecinos del barrio La Cava.
                         </p>
                     </Carousel.Caption>
