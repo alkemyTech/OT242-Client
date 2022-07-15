9eeed8a1d21a783d19f7af29969d503e6eeb17b8
Rafael Rodriguez Viau <rodriguezviaurafael@gmail.com>
2022-07-15 11:37:53 -0300

    DELETED useless files

--- src/UI/UIRegistration.js
+++ src/UI/UIRegistration.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import RegistrationForm from '../components/RegistrationForm';
+import RegistrationForm from '../Components/RegistrationForm';
 import '../UI/CSSRegistration.css';
 
 function Registration(
@@ -11,7 +11,9 @@ function Registration(
             <h1>Crea tu cuenta</h1>
             <RegistrationForm />
         </div>
-        <div className="Half_div"><img className='Main_img' src={require('../Resources/Piled_hands_img.jpg')} alt="Piled_hands_img" /></div>
+        <div className="Half_div"><img className='Main_img' 
+            src="https://thumbs.dreamstime.com/b/hands-stacked-pile-symbol-teamwork-trust-business-people-87756917.jpg"
+            alt="Piled_hands_img" /></div>
     </div>
     
     );
