dcb1b03ee6a427975f71c47eba7846a78849f5ba
Gianluca <gianpetrii@gmail.com>
2022-07-21 11:49:58 -0300

    Commit inicial probando el layout

--- src/Components/BackOffice/BackOffice.jsx
+++ src/Components/BackOffice/BackOffice.jsx
@@ -1,9 +1,59 @@
+// dependencias
 import React from "react";
 
+// estilos
+import './BackOffice.css';
+
+// llamo a api para traer get req de toda la info de todos
+// lo guardo todo en una lista para cada seccion como [titulo, descripcion y imagen],[]....
+// renderizo cada uno como un card
+
+
 function BackOffice() {
+
+  // listado con cada seccion a mostrar
+  const secciones = ["Bienvenidos", "Nosotros", "Novedades", "Testimonios", "Equipo"]
+
+
+
   return (
     <>
-      <h1>Hola soy el BackOffice</h1>
+      <div className="row">
+        <div className="column">
+          <div className="card">
+            <h3>Card 1</h3>
+            <p>Some text</p>
+            <p>Some text</p>
+          </div>
+        </div>
+
+        <div className="column">
+          <div className="card">
+            <h3>Card 2</h3>
+            <p>Some text</p>
+            <p>Some text</p>
+          </div>
+        </div>
+        
+        <div className="column">
+          <div className="card">
+            <h3>Card 3</h3>
+            <p>Some text</p>
+            <p>Some text</p>
+          </div>
+        </div>
+        
+        <div className="column">
+          <div className="card">
+            <h3>Card 4</h3>
+            <p>Some text</p>
+            <p>Some text</p>
+          </div>
+        </div>
+      </div>
+      
+
+      
     </>
   );
 }
