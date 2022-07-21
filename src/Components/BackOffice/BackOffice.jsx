750b2091ac79ab6d5720268a4c2f6f9a789ae75b
Gianluca <gianpetrii@gmail.com>
2022-07-21 16:29:00 -0300

    Layout Back Office finalizado, rutas peuden variar

--- src/Components/BackOffice/BackOffice.jsx
+++ src/Components/BackOffice/BackOffice.jsx
@@ -1,5 +1,16 @@
+// archivos
+
+
 // dependencias
 import React from "react";
+import { FaRegNewspaper, FaRegUser } from "react-icons/fa"; // icono novedades, miembros
+import { TbListCheck } from "react-icons/tb";  // icono actividades
+import { BiCategory, BiMessageDetail, BiSlideshow } from "react-icons/bi";  // icono categorias, testimonios, slides
+import { RiOrganizationChart } from "react-icons/ri"; // icono organixzacion
+import { FiUsers } from "react-icons/fi"; // icono usuarios
+import { Link } from 'react-router-dom'; // uso los links para redirigir
+
+
 
 // estilos
 import './BackOffice.css';
@@ -11,49 +22,33 @@ import './BackOffice.css';
 
 function BackOffice() {
 
-  // listado con cada seccion a mostrar
-  const secciones = ["Bienvenidos", "Nosotros", "Novedades", "Testimonios", "Equipo"]
 
+  // listado con cada seccion a mostrar
+  const secciones = [
+    ["Novedades", <FaRegNewspaper className="backOffIcon" />, "/novedades"], 
+    ["Actividades", <TbListCheck className="backOffIcon" />, "/actividades"],
+    ["Categorias", <BiCategory className="backOffIcon" />, "/categorias"],
+    ["Testimonios", <BiMessageDetail className="backOffIcon" />, "/testimonios"],
+    ["Organizacion", <RiOrganizationChart className="backOffIcon" />, "/organizacion"],
+    ["Slides", <BiSlideshow className="backOffIcon" />, "/slides"],
+    ["Usuarios", <FaRegUser className="backOffIcon" />, "/usuarios"],
+    ["Miembros", <FiUsers className="backOffIcon" />, "/miembros"]
+  ]
 
 
   return (
     <>
-      <div className="row">
-        <div className="column">
-          <div className="card">
-            <h3>Card 1</h3>
-            <p>Some text</p>
-            <p>Some text</p>
-          </div>
-        </div>
-
-        <div className="column">
-          <div className="card">
-            <h3>Card 2</h3>
-            <p>Some text</p>
-            <p>Some text</p>
-          </div>
-        </div>
-        
-        <div className="column">
-          <div className="card">
-            <h3>Card 3</h3>
-            <p>Some text</p>
-            <p>Some text</p>
-          </div>
-        </div>
-        
-        <div className="column">
-          <div className="card">
-            <h3>Card 4</h3>
-            <p>Some text</p>
-            <p>Some text</p>
-          </div>
-        </div>
-      </div>
-      
-
-      
+      <body className="backOffice">
+        {secciones.map(seccion => (
+            <div className="backOffColumn" key={seccion[0]}>
+              <div className="backOffCard" >
+                <h3>{seccion[0]}</h3>
+                <div>{seccion[1]}</div>
+                <Link to={seccion[2]}><button className="backOffButton">Ir</button></Link>
+              </div>
+            </div>
+        ))}
+      </body>
     </>
   );
 }
