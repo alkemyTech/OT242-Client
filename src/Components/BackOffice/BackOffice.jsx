03d39a78e60e7ef2a027944231bf554ed02afd8d
Gianluca <gianpetrii@gmail.com>
2022-07-22 19:59:50 -0300

    Creado de Slicer e importado al componente
    Olvide hacer commits mas chicos, pero aca se crea el <slicer> folder,
    dentro llamo a mi reducer userLogged y lo defino dentro del store.
    Tambien creo la funcion que haria la peticion al backend con axios y
    en el BackOffice component actualizo el estado con la accion y lo tengo
    listo para mostrar.
    Ahora me queda hacer una comparacion del rol para ver que renderizar

---
+++ src/Components/BackOffice/BackOffice.jsx
@@ -0,0 +1,74 @@
+// archivos
+
+
+// dependencias
+import React, { useEffect } from "react";
+import { FaRegNewspaper, FaRegUser } from "react-icons/fa"; // icono novedades, miembros
+import { TbListCheck } from "react-icons/tb";  // icono actividades
+import { BiCategory, BiMessageDetail, BiSlideshow } from "react-icons/bi";  // icono categorias, testimonios, slides
+import { RiOrganizationChart } from "react-icons/ri"; // icono organixzacion
+import { FiUsers } from "react-icons/fi"; // icono usuarios
+import { Link } from 'react-router-dom'; // uso los links para redirigir
+
+// redux
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUserLogged } from "../../app/slices/userLogged/index";
+
+
+
+
+// estilos
+import './BackOffice.css';
+
+// llamo a api para traer get req de toda la info de todos
+// lo guardo todo en una lista para cada seccion como [titulo, descripcion y imagen],[]....
+// renderizo cada uno como un card
+
+
+function BackOffice() {
+  // para actualizar store y renderizar el state actualizado
+  const dispatch = useDispatch();
+
+  // dentro de los reducers solo uso el que se llama user y me traigo la list que defini en initialState y le cambio el nombre
+  const { currentUser } = useSelector(state => state.userLogged);
+  console.log(currentUser);
+
+  // traigo del store el listado de usuarios
+  
+  useEffect(() => {
+    dispatch(fetchUserLogged());
+  }, [dispatch])
+
+
+  // listado con cada seccion a mostrar
+  const secciones = [
+    ["Novedades", <FaRegNewspaper className="backOffIcon" />, "/novedades"], 
+    ["Actividades", <TbListCheck className="backOffIcon" />, "/actividades"],
+    ["Categorias", <BiCategory className="backOffIcon" />, "/categorias"],
+    ["Testimonios", <BiMessageDetail className="backOffIcon" />, "/testimonios"],
+    ["Organizacion", <RiOrganizationChart className="backOffIcon" />, "/organizacion"],
+    ["Slides", <BiSlideshow className="backOffIcon" />, "/slides"],
+    ["Usuarios", <FaRegUser className="backOffIcon" />, "/usuarios"],
+    ["Miembros", <FiUsers className="backOffIcon" />, "/miembros"]
+  ]
+
+
+  return (
+    <>
+      <body className="backOffice">
+        <h1>{currentUser.rol}</h1>
+        {secciones.map(seccion => (
+            <div className="backOffColumn" key={seccion[0]}>
+              <div className="backOffCard" >
+                <h3>{seccion[0]}</h3>
+                <div>{seccion[1]}</div>
+                <Link to={seccion[2]}><button className="backOffButton">Ir</button></Link>
+              </div>
+            </div>
+        ))}
+      </body>
+    </>
+  );
+}
+
+export default BackOffice;
