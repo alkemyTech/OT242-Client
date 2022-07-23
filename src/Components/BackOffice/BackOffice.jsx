9b9cc4892d78a877bb7726c18a3ea7ea84220ff0
Gianluca <gianpetrii@gmail.com>
2022-07-23 19:59:09 -0300

    Chequeo de token para restringir acceso

--- src/Components/BackOffice/BackOffice.jsx
+++ src/Components/BackOffice/BackOffice.jsx
@@ -8,7 +8,7 @@ import { TbListCheck } from "react-icons/tb";  // icono actividades
 import { BiCategory, BiMessageDetail, BiSlideshow } from "react-icons/bi";  // icono categorias, testimonios, slides
 import { RiOrganizationChart } from "react-icons/ri"; // icono organixzacion
 import { FiUsers } from "react-icons/fi"; // icono usuarios
-import { Link } from 'react-router-dom'; // uso los links para redirigir
+import { Link, Navigate } from 'react-router-dom'; // uso los links para redirigir
 
 import { BiEditAlt } from 'react-icons/bi';
 
@@ -28,6 +28,7 @@ import './BackOffice.css';
 
 
 function BackOffice() {
+  let token = sessionStorage.getItem('token');
 
   // para actualizar store y renderizar el state actualizado
   const dispatch = useDispatch();
@@ -61,30 +62,34 @@ function BackOffice() {
 
   return (
     <>
-      <body className="backOffice">
-        {rol === 'administrador' ? 
-          <>
-          {secciones.map(seccion => (
-              <div className="backOffColumn" key={seccion[0]}>
-                <div className="backOffCard" >
-                  <h3>{seccion[0]}</h3>
-                  <div>{seccion[1]}</div>
-                  <Link to={seccion[2]}><button className="backOffButton">Ir</button></Link>
-                </div>
-              </div>
-          ))}
-          </>
-          : <>
-            <div className="backOffColumn">
-              <div className="backOffCard">
-                  <h3>Edit</h3>
-                  <div><BiEditAlt className="backOffIcon"/></div>
-                  <Link to="/userInfo"><button className="backOffButton">Ir</button></Link>
-                </div>
-              </div>
-          </>
-        }
-      </body>
+      {token === false ? <Navigate to="/" /> :
+        <>
+          <div className="backOffice">
+            {rol === 'administrador' ? 
+              <>
+                {secciones.map(seccion => (
+                    <div className="backOffColumn" key={seccion[0]}>
+                      <div className="backOffCard" >
+                        <h3>{seccion[0]}</h3>
+                        <div>{seccion[1]}</div>
+                        <Link to={seccion[2]}><button className="backOffButton">Ir</button></Link>
+                      </div>
+                    </div>
+                ))}
+              </>
+              : <>
+                  <div className="backOffColumn">
+                  <div className="backOffCard">
+                      <h3>Edit</h3>
+                      <div><BiEditAlt className="backOffIcon"/></div>
+                      <Link to="/userInfo"><button className="backOffButton">Ir</button></Link>
+                    </div>
+                  </div>
+                </>
+            }
+          </div>
+        </>
+      }
     </>
   );
 }
