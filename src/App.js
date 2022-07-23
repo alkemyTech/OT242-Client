2cdd57a8712bcc4db2befadac0b7fe07f0953c43
Gianluca <gianpetrii@gmail.com>
2022-07-23 13:11:11 -0300

    Arregle borrado erroneo de App.js

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+import Registration from './UI/registration/UIRegistration';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Nav from './components/Nav';
+import AboutUsPage from "./pages/AboutUsPage";
+import ActivitiesPage from "./pages/ActivitiesPage";
+import ContactPage from "./pages/ContactPage/ContactPage";
+import Home from './UI/UIHome.js';
+
+function App() {
+  return (
+
+    <div className="App">
+     <BrowserRouter>
+    <Nav></Nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/AboutUs" element={<AboutUsPage />} />
+          <Route path="/Activities" element={<ActivitiesPage />} />
+          <Route path="/Contact" element={<ContactPage />} />
+          <Route path="*" element={<Navigate replace to="/" />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
