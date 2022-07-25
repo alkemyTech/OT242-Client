54af6e5be13773c0e73d54015b401ee4f9e28123
manubautista <manubautista2009@gmail.com>
2022-07-24 21:20:35 -0300

    DetailedNew component and Route added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 import Registration from './UI/registration/UIRegistration';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Nav from './components/Nav';
+import Nav from './Components/Nav';
 import AboutUsPage from "./pages/AboutUsPage";
 import ActivitiesPage from "./pages/ActivitiesPage";
 import ContactPage from "./pages/ContactPage/ContactPage";
 import Home from './UI/UIHome.js';
+import DetailedNew from './pages/Novedades/DetailedNew';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
           <Route path="/Activities" element={<ActivitiesPage />} />
           <Route path="/Contact" element={<ContactPage />} />
           <Route path="*" element={<Navigate replace to="/" />} />
+          <Route path="/novedad/:id" element={<DetailedNew/>} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/Novedades/DetailedNew.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useParams, useNavigate } from "react-router-dom";
+import Swal from 'sweetalert2'
+
+const DetailedNew = () => {
+  const URI = "http://localhost:3000/novedad/";
+  const { id } = useParams();
+  const navigate = useNavigate()
+
+  const [novedad, setNovedad] = React.useState([]);
+  
+  React.useEffect(() => {
+    obtenerDatos();
+  }, []);
+
+  const obtenerDatos = async () => {
+    try {
+        const res = await axios.get(`${URI}${id}`);
+        setNovedad(res.data);
+    } catch (err) {
+        Swal.fire('Error 404', 'Esta novedad no existe', 'error')
+        navigate('/')
+    }
+    
+  };
+
+  return (
+    <div>
+      <h1>Detalle de novedad con id {id}</h1>
+      <h2>{novedad.name}</h2>
+      <h5>{novedad.type}</h5>
+      <img src={novedad.image}></img>
+      <h3>{novedad.content}</h3>
+    </div>
+  );
+};
+
+export default DetailedNew;
