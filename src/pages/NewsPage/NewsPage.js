fb2d3ed8f0d11188c065490e682b215ba119ab38
Gianluca <gianpetrii@gmail.com>
2022-07-25 11:49:39 -0300

    Llamado a la api y redirect a NewsDetails

--- src/pages/NewsPage/NewsPage.js
+++ src/pages/NewsPage/NewsPage.js
@@ -8,6 +8,7 @@ import React, { useEffect, useState } from 'react'
 
 // dependencies
 import { Link } from 'react-router-dom'
+import axios from 'axios';
 
 
 // this is an array created by me to do the tests, DO NOT UNCOMMENT
@@ -17,8 +18,21 @@ import { Link } from 'react-router-dom'
 const NewsPage = () => {
 
   // declaring data variable
-  const [data, setData] = useState([]);
+  const [news, setNews] = useState([]);
 
+  // traigo listado de novedades del backend
+  useEffect(() => {   
+    axios
+        .get("http://localhost:8080/news")  //chequear si el path es correcto
+        .then(res => {
+            console.log(res);
+            setNews(res.data);
+        })
+        .catch(err => {
+            console.log(err)
+        }
+    )
+}, []);     //la lista vacia es para que solo haga la accion una vez
   
 
   return (
@@ -29,14 +43,14 @@ const NewsPage = () => {
 
           <ul className="card_news" >
 
-            {/* {data?.map(data => (
-                  <li key={data.id}>
-                    <Link to={`/news/${data.id}`}>
-                        <img src={data.image} width={300}/>
-                        <h3 className="title_new">{data.title}</h3>
+            {news.map(oneNews => (
+                  <li key={oneNews.id}>
+                    <Link to={`/news/${oneNews.id}`}>
+                        <img src={oneNews.image} width={300} />
+                        <h3 className="title_new">{oneNews.title}</h3>
                     </Link>
                   </li>
-            ))} */}
+            ))}
 
           </ul>
 
