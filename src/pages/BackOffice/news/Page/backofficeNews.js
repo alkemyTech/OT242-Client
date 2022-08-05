c7c490f3a2727709a44dd51f4465b26e1f4f9d09
galarzahys <galarza.hys@gmail.com>
2022-08-05 09:11:35 -0300

    DeleteCat component was deleted

--- src/pages/BackOffice/news/Page/backofficeNews.js
+++ src/pages/BackOffice/news/Page/backofficeNews.js
@@ -1,75 +1,70 @@
-import { useState, useEffect } from 'react';
-import NewsItem from '../../../../components/backNewsList/NewsItem';
-import React from 'react';
-import './backofficeNews.css'
-import { getReq } from '../../../../helpers/ReqToApi';
-import Button from '../../../../components/buttons/Button';
-import { DeleteCat } from '../../../../components/backNewsList/DeleteCat';
-
-
-
-
+import { useState, useEffect } from "react";
+import NewsItem from "../../../../components/backNewsList/NewsItem";
+import React from "react";
+import "./backofficeNews.css";
+import { getReq } from "../../../../helpers/ReqToApi";
+import Button from "../../../../components/buttons/Button";
+import { DeleteCat } from "../../../../components/backNewsList/DeleteCat";
 
 const BackNewsPage = (props) => {
-    const [loading, setLoading] = useState(false);
-    const [news, setNews] = useState([]);
-    const [cats, setCats] = useState([]);
-    const [value, setValue] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [news, setNews] = useState([]);
+  const [cats, setCats] = useState([]);
+  const [value, setValue] = useState("");
 
-    useEffect (() => {
-      const loadNews = async () => {
-        setLoading (true);
-        const response = await getReq(`/admin/news`);
-        setNews(response.data);
-        setLoading(false);
-      };
+  useEffect(() => {
+    const loadNews = async () => {
+      setLoading(true);
+      const response = await getReq(`/admin/news`);
+      setNews(response.data);
+      setLoading(false);
+    };
 
-      loadNews();
+    loadNews();
+  }, []);
 
-    }, []);
-
-    useEffect (() => {
-      const loadCats = async () => {
-        setLoading (true);
-        const response = [{
+  useEffect(() => {
+    const loadCats = async () => {
+      setLoading(true);
+      const response = [
+        {
           id: 1,
-          name: 'Donaciones'
+          name: "Donaciones",
         },
         {
           id: 2,
-          name: 'Actividades'
+          name: "Actividades",
         },
         {
           id: 3,
-          name: 'Voluntariados'
-        }];
-        setCats(response);
-        setLoading(false);
-      };
+          name: "Voluntariados",
+        },
+      ];
+      setCats(response);
+      setLoading(false);
+    };
 
-      loadCats();
+    loadCats();
+  }, []);
 
-    }, []);
-
-    const onSubmit= () => {
-
-      let id = value.split(" ")[0]
-      let name = value.split(" ")[1]
-      console.log(id)
-      let msg = "La categoria fue eliminada correctamente";
-      if(window.confirm(`Esta seguro que desea eliminar la categoria: ${name}`));
-      try {
-       //let del = await postReq (`/admin/categories/${id}`)
-       console.log(msg);          
-     }catch (err) {
+  const onSubmit = () => {
+    let id = value.split(" ")[0];
+    let name = value.split(" ")[1];
+    console.log(id);
+    let msg = "La categoria fue eliminada correctamente";
+    if (window.confirm(`Esta seguro que desea eliminar la categoria: ${name}`));
+    try {
+      //let del = await postReq (`/admin/categories/${id}`)
+      console.log(msg);
+    } catch (err) {
       console.log(err);
-    }}
-
+    }
+  };
 
   return (
     <section className="holder">
       <h2>Listado de Novedades</h2>
-      <table className='news-table'>
+      <table className="news-table">
         <thead>
           <th>Nombre</th>
           <th>Imagen</th>
@@ -79,29 +74,45 @@ const BackNewsPage = (props) => {
         {loading ? (
           <p>Cargando...</p>
         ) : (
-            news.map(item => <NewsItem key={item.id}
-              name={item.name} image={item.image}
-              createdAt={item.createdAt.slice(0,10)} />)
+          news.map((item) => (
+            <NewsItem
+              key={item.id}
+              name={item.name}
+              image={item.image}
+              createdAt={item.createdAt.slice(0, 10)}
+            />
+          ))
         )}
-       </table>
-       <div>
+      </table>
+      <div>
         <h5>Eliminar categorias de novedades</h5>
-       <form>
-       <select value={ value } onChange={ (event) => setValue(event.target.value)} >
-       {loading ? (
-          <p>Cargando...</p>
-        ) : ( 
-            cats.map(cat => <DeleteCat key={cat.id}
-              name={cat.name} id={cat.id} />)
-
-        )}
-       
-        </select>
-        <Button type={"Button"} onClick={onSubmit} className={"button-secondary"} text={"Eliminar"}></Button>
-
+        <form>
+          <select
+            value={value}
+            onChange={(event) => setValue(event.target.value)}
+          >
+            {loading ? (
+              <p>Cargando...</p>
+            ) : (
+              cats.map((cat) => {
+                return (
+                  <>
+                    <option key={cat.id} value={`${cat.id} ${cat.name}`}>
+                      {cat.name}
+                    </option>
+                  </>
+                );
+              })
+            )}
+          </select>
+          <Button
+            type={"Button"}
+            onClick={onSubmit}
+            className={"button-secondary"}
+            text={"Eliminar"}
+          ></Button>
         </form>
-        </div>
-        
+      </div>
     </section>
   );
 };
