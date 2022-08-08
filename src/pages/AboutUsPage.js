11bfbf7521f8c7ae42deff597cff4506d77eb3dd
galarzahys <galarza.hys@gmail.com>
2022-08-08 15:39:23 -0300

    Add aboutus content

--- src/pages/AboutUsPage.js
+++ src/pages/AboutUsPage.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
+import MemberCard from '../../components/cards/members_card/MemberCard';
+import getReq from '../helpers/ReqToApi'
+
 
 const AboutUsPage = (props) => {
+
+
+    const [loading, setLoading] = useState(false);
+    const [members, setMembers] = useState([]);
+
+    useEffect (() => {
+      const loadMembers = async () => {
+        setLoading (true);
+        const response = await getReq(`/admin/news`);
+        setMembers(response);
+        setLoading(false);
+      };
+
+      loadMembers();
+
+    }, []);
+
+
     return (
         <div>
+
+        {loading ? (
+          <p>Cargando...</p>
+        ) : (
+            members.map(item => <MemberCard key={item.id}
+              name={item.name} image={`../../public/images/Miembros_del_equipo/${item.name}.jpg`}
+               />)
+        )}
            <h1>test</h1>
         </div>
     )
--- src/pages/ContactPage/ContactPage.js
+++ src/pages/ContactPage/ContactPage.js
@@ -1,39 +1,10 @@
-
-import { React, useEffect, useState } from 'react';
-import MemberCard from '../../components/cards/members_card/MemberCard';
+import { React } from 'react';
 import ContactForm from '../../components/forms/Contact_form';
-import { getReq } from '../../helpers/ReqToApi';
 import '../ContactPage/ContactPage.css'
 
 const ContactPage = (props) => {
 
-    const [loading, setLoading] = useState(false);
-    const [members, setMembers] = useState([]);
 
-    useEffect (() => {
-      const loadMembers = async () => {
-        setLoading (true);
-        //const response = await getReq(`/admin/news`);
-        const response = [
-          {
-            id: 1,
-            name: "Gonzalo",
-            image: 'https://fotografias.lasexta.com/clipping/cmsimages01/2021/11/04/0D7921CD-4ABE-4B8F-9F2D-961C98060E86/98.jpg?crop=3500,1969,x0,y0&width=1900&height=1069&optimize=low&format=webply',
-          },
-          {
-            id: 2,
-            name: "David",
-            image: './public/images/Miembros del Equipo/Rodrigo Fuente.jpg',
-          },
-
-        ]
-        setMembers(response);
-        setLoading(false);
-      };
-
-      loadMembers();
-
-    }, []);
 
 
   return (
@@ -41,13 +12,7 @@ const ContactPage = (props) => {
       <div className="text_contact">
         <h2>Texto aleatorio</h2>
 
-        {loading ? (
-          <p>Cargando...</p>
-        ) : (
-            members.map(item => <MemberCard key={item.id}
-              name={item.name} image={item.image}
-               />)
-        )}
+        
       </div>
         
       <ContactForm />
