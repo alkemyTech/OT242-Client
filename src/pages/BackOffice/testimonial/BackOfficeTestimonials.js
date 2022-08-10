b6632f4ca9f4f4a206b052c42b4ae0c65cfecbaf
galarzahys <galarza.hys@gmail.com>
2022-08-10 10:11:11 -0300

    path problem was resolved

--- src/pages/BackOffice/testimonial/BackOfficeTestimonials.js
+++ src/pages/BackOffice/testimonial/BackOfficeTestimonials.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import React from 'react';
 import './testimonials.css'
 import { getReq } from '../../../helpers/ReqToApi';
-import TestimonialItem from '../../../components/BackOffice/BackOfficeUsers/TestimonialItem';
+import TestimonialItem from '../../backOffice/BackOfficeUsers/TestimonialItem';
 
 const BackTestimonialPage = (props) => {
     const [loading, setLoading] = useState(false);
