da835ab2b993b2110865a773d83d6a2cbbec7c5a
Rafaviau <86501817+Rafaviau@users.noreply.github.com>
2022-08-10 09:51:47 -0300

    Update NewsPage.js

--- src/pages/public/newsPage/NewsPage.js
+++ src/pages/public/newsPage/NewsPage.js
@@ -26,7 +26,6 @@ const NewsPage = () => {
 
   return (
     <div className="container_news">
-<<<<<<< HEAD:src/pages/NewsPage/NewsPage.js
       <h1 className="NewsTitle">Novedades</h1>
       <div className="container">
         <div className="row">
@@ -38,28 +37,8 @@ const NewsPage = () => {
               </Link>
             </div>
           ))}
-
-
         </div>
       </div>
-=======
-          {/* replacement by request to the news service,
-          I'm mapping an array I created, so it should be changed when it's ready.*/}
-
-
-          <ul className="card_news" >
-
-            {news.map(oneNews => (
-                  <li key={oneNews.id}>
-                    <Link to={`/news/${oneNews.id}`}>
-                        <img src={oneNews.image} width={300} alt={oneNews.name} />
-                        <h3 className="title_new">{oneNews.name}</h3>
-                    </Link>
-                  </li>
-            ))}
-
-          </ul>
->>>>>>> main:src/pages/public/newsPage/NewsPage.js
 
     </div>
   )
