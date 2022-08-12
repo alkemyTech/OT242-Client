025f377228b2a7d5f29f51394e616fdcc2bd7dce
Gianluca <gianpetrii@gmail.com>
2022-08-12 01:33:55 -0300

    Arreglo de estilos de contact form

--- src/components/forms/contactForm/ContactForm.jsx
+++ src/components/forms/contactForm/ContactForm.jsx
@@ -1,8 +1,14 @@
+// react
 import React, { useState } from "react";
+
+// files, components and functions
+import AlertForm from "../../alerts/AlertForm";
+import { postReq } from "../../../helpers/ReqToApi";
+import Success from "../../alerts/Success";
+import './ContactForm.css';
+
+//dependencies
 import { Formik, Form, Field } from "formik";
-import AlertForm from "../alerts/AlertForm";
-import { postReq } from "../../helpers/ReqToApi";
-import Success from "../alerts/Success";
 import * as Yup from 'yup';
 
 
@@ -76,50 +82,50 @@ const ContactForm = () => {
           isSubmitting,
         }) => (
 
-          <Form className="form">
-                <h2>Pongase en contacto con nosotros</h2>
+          <Form className="contactForm">
+                <h2 className="contactTitle">Contactate con nosotros!</h2>
                <Field 
-               className = "form_input"
+               className = "contactInput"
                name="name"
                type="name"
-               placeholder="Ingrese su nombre aqui"
-                /> 
+               placeholder="Nombre y Apellido"
+                /> <br /><br />
               {errors.name && touched.name ? (
              <AlertForm error={errors.name}/>
              ) : null}
 
               <Field 
-               className = "form_input"
+               className = "contactInput"
                name="phone"
                type="text"
-               placeholder="Ingrese un telefono de contacto"
-                /> 
+               placeholder="Telefono"
+                /> <br /><br />
               {errors.phone && touched.phone ? (
              <AlertForm error={errors.phone}/>
              ) : null}
 
                <Field 
-               className = "form_input"
+               className = "contactInput"
                name="email"
                type="email"
-               placeholder="Ingrese una direccion de e-mail"
-                /> 
+               placeholder="Email"
+                /> <br /><br />
               {errors.email && touched.email ? (
              <AlertForm error={errors.email}/>
              ) : null}
 
                <Field 
-               className = "form_input text"
+               className = "contactText"
                name="message"
                type="message"
-               placeholder="Escriba aqui su mensaje"
-                /> 
+               placeholder="Escribe tu consulta"
+                /> <br /><br />
               {errors.message && touched.message ? (
              <AlertForm error={errors.message}/>
              ) : null}
 
-          <button type="submit" disabled={isSubmitting} className="submit_btn">
-             Enviar
+          <button type="submit" disabled={isSubmitting} className="contactBtn">
+             Enviar consulta
           </button>
 
           <div>
--- src/pages/public/contactPage/ContactPage.js
+++ src/pages/public/contactPage/ContactPage.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import ContactForm from '../../../components/forms/Contact_form';
+import ContactForm from '../../../components/forms/contactForm/ContactForm';
 import './ContactPage.css'
 
-const ContactPage = (props) => {
-
-
-
+const ContactPage = () => {
 
   return (
-    <main className="main_contact">
-      <div className="text_contact">
-        <h2>Texto aleatorio</h2>
-
-        
-      </div>
-        
+    <section className="contactPage">
+                
       <ContactForm />
 
-
-    </main>
+    </section>
   );
 };
 
