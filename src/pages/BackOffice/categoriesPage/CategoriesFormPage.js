1c97e06fc5d617393f051ff05f09fc881c6fdc45
mcamimejia <mcamila.mejia@outlook.com>
2022-08-19 13:12:09 -0500

    categories backoffice

---
+++ src/pages/BackOffice/categoriesPage/CategoriesFormPage.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { getReq } from '../../../helpers/ReqToApi'
+
+import CategoriesForm from '../../../components/forms/CategoriesForm/CategoriesForm'
+
+
+const CategoriesFormPage = () => {
+    const [ categories, setCategories] = useState([]);
+    const {id} = useParams();
+    const getData = async () => {
+        const {data} = await getReq(`/categories/${id}`);
+        setCategories(data);
+    };
+
+    useEffect(() => {
+        getData();
+    }, [])
+
+
+
+  return (
+    <div className="container_news">
+      <CategoriesForm
+      categories = {categories} />
+    </div>
+  )
+}
+
+export default CategoriesFormPage
